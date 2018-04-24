1bdf56b4534cd5bcccca730ed4c3a2a7bb00a945
Aurélien GALTIER <aurelien.galtier@live.fr>
2018-04-24 13:22:36 +0200

    Update of BotBuilder

--- BotFramework.Topics/ConversationState.cs
+++ BotFramework.Topics/ConversationState.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.Bot.Builder.Core.Extensions;
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Core.Extensions;
 
 namespace BotFramework.Topics
 {
-    public class ConversationState<T> : StoreItem 
+    public class ConversationState<T>
         where T : ITopicBotContext<T>
     {
+        public ConversationState()
+        {
+            this.Previous = new List<ITopic<T>>();
+        }
+
         public ITopic<T> ActiveTopic { get; set; }
+        public List<ITopic<T>> Previous { get; set; }
     }
 }
